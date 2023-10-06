48581752919ba9b92b3f82dd46bd5629dd017554
Mucahid Alkan <mucahid.alkan0310@gmail.com>
2023-10-06 15:45:09 +0200

    muziek gelinked

--- source/albums.php
+++ source/albums.php
@@ -3,60 +3,70 @@
 // Gegevens van muziek singles
 $musicAlbums = [
     [
+        "id" => "1",
         "title" => "Starboy ft. Daft Punk",
         "artist" => "Weekend",
         "genre" => "HipHop",
         "image" => "/img/album1.jpg"
     ],
     [
+        "id" => "2",
         "title" => "The Weeknd - Often (NSFW)",
         "artist" => "Weekend",
         "genre" => "HipHop",
         "image" => "/img/often.jpg"
     ],
     [
+        "id" => "3",
         "title" => "The Weeknd - Save Your Tears",
         "artist" => "Weekend",
         "genre" => "HipHop",
         "image" => "/img/save.jpeg"
     ],
     [
+        "id" => "4",
         "title" => "The Weeknd - Blinding Lights",
         "artist" => "Weekend",
         "genre" => "HipHop",
         "image" => "/img/blin.jpg"
     ],
     [
+        "id" => "5",
         "title" => "Michael Jackson - Smooth Criminal",
         "artist" => "Michael Jackson",
         "genre" => "HipHop",
         "image" => "/img/smooth.jpg"
     ],
     [
+        "id" => "6",
         "title" => "Michael Jackson - Billie Jean",
         "artist" => "Michael Jackson",
         "genre" => "HipHop",
         "image" => "/img/micheal.jpg"
     ],
     [
+        "id" => "7",
         "title" => "Michael Jackson - They Don’t Care About Us",
         "artist" => "Michael Jackson",
         "genre" => "Pop",
         "image" => "/img/they.webp"
     ],
     [
+        "id" => "8",
         "title" => "Pop Smoke - For The Night",
         "artist" => "Pop Smoke",
         "genre" => "Rap",
         "image" => "/img/for.jpg"
     ],
     [
+        "id" => "9",
         "title" => "POP SMOKE - INVINCIBLE",
         "artist" => "Pop Smoke",
         "genre" => "HipHop",
         "image" => "/img/pop.jpg"
     ],
     [
+        "id" => "10",
         "title" => "POP SMOKE - ELEMENT",
         "artist" => "Pop Smoke",
         "genre" => "Rap",
--- source/views/albums1.php
+++ source/views/albums1.php
@@ -1,12 +1,13 @@
 <div class="albums">
-    <img src="<?= $albums["image"] ?>" alt="<?= $albums["title"] ?>">
-    <h3>
-        <?= $albums["title"] ?>
-    </h3>
-    <p>Artiest:
-        <?= $albums["artist"] ?>
-    </p>
-    <p>Genre:
-        <?= $albums["genre"] ?>
-    </p>
+<a href="muziek.php?id=<?= $albums["id"] ?>" class="single">
+        <img src="<?= $albums["image"] ?>" alt="<?= $albums["title"] ?>">
+        <h3>
+            <?= $albums["title"] ?>
+        </h3>
+        <p>Artiest:
+            <?= $albums["artist"] ?>
+        </p>
+        <p>Genre:
+            <?= $albums["genre"] ?>
+        </p>
 </div>
